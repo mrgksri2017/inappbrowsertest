5ae4d009eadd097bf636597369d623c7b7700d85
mrgksri2017 <mrgksri@gmail.com>
2022-08-04 13:39:20 +0530

    inbrowserapp test with getmehub

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private iab: InAppBrowser,) {
+
+  }
+
+  openBlank() {
+    this.iab.create(`https://www.getmehub.com`, `_blank`);
+  }
+
+
+  openSystem() {
+    this.iab.create(`https://www.getmehub.com`, `_system`);
+  }
+
+}
